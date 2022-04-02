93c6c22fe2bfa71c9ef4b6bc56afd3acf4a3981a
Stefano Cipolletta <stefano.cipolletta@edu.unito.it>
2022-04-02 16:13:55 +0200

    Sorting Header file with quickSort, partition and swap headers

--- Exercise 1/src/sorting.h
+++ Exercise 1/src/sorting.h
@@ -9,4 +9,32 @@
 
 #include "generic_array.h"
 
+/**
+ * Orders the array using the quick sort algorithm
+ * @param unsorted_array unsorted array
+ * @param precedence pointer to a function which determines the precedence relation between the array elements.
+ * It returns 1 iff the first element is greater than the second.
+ * It returns -1 iff the first element is smaller than the second.
+ * It return 0 iff the first and the second elements are equal.
+ * @pre GenericArray cannot be NULL
+ * @pre *precedence must be a reference to a valid function
+ * @author Stefano Cipolletta
+ * */
+void quick_sort(GenericArray* unsorted_array, int (*precedence)(void*, void*));
+
+/**
+ * Hearth of the QuickSort algorithm
+ * @param unsorted_array unsorted array
+ * @param precedence pointer to a function which determines the precedence relation between the array elements.
+ * It returns 1 iff the first element is greater than the second.
+ * It returns -1 iff the first element is smaller than the second.
+ * It return 0 iff the first and the second elements are equal.
+ * @param first index of the first element in the range to partition
+ * @param last index of the last element in the range to partition
+ * @author Stefano Cipolletta
+ * */
+unsigned long partition(GenericArray* unsorted_array, int (*precedence)(void*, void*), unsigned long first, unsigned long last);
+
+void swap(GenericArray* unsorted_array, unsigned long index1, unsigned long index2);
+
 #endif /*SORTING_H_adsnoiqpfmoa*/
