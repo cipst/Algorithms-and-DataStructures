0bfcf5e684d5c1812da9514025c68189b8c39fc2
Stefano Cipolletta <stefano.cipolletta@edu.unito.it>
2022-04-02 12:49:50 +0200

    Struct GenericArray & generic_array_create()

--- Exercise 1/src/generic_array.c
+++ Exercise 1/src/generic_array.c
@@ -1,4 +1,43 @@
+/**
+ * @file generic_array.c
+ * @author Stefano Cipolletta
+ * @version v0.1
+ * */
+
 #include "generic_array.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifndef INITIAL_ARRAY_SIZE
+#define INITIAL_ARRAY_SIZE 10
+#endif
+
+struct _GenericArray {
+    void** array;
+    unsigned long num_el;
+    unsigned long size;
+};
+
+GenericArray* generic_array_create() {
+    GenericArray* array_ptr = (GenericArray*)malloc(sizeof(GenericArray));
+    if (array_ptr == NULL) {
+        fprintf(stderr, "generic_array_create(): Unable to allocate memory for the new array.");
+        exit(EXIT_FAILURE);
+    }
+
+    array_ptr->array = malloc(INITIAL_ARRAY_SIZE * sizeof(void*));
+    if (array_ptr->array == NULL) {
+        fprintf(stderr, "generic_array_create(): Unable to allocate memory for the new array.");
+        exit(EXIT_FAILURE);
+    }
+
+    for (unsigned long i = 0; i < INITIAL_ARRAY_SIZE; ++i) {
+        array_ptr->array[i] = NULL;
+    }
+
+    array_ptr->num_el = 0;
+    array_ptr->size = INITIAL_ARRAY_SIZE;
+
+    return (array_ptr);
+}
