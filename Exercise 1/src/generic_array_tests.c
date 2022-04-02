3e1766bbb7394c07864fbf895e0cdfe15e38b6e1
Stefano Cipolletta <stefano.cipolletta@edu.unito.it>
2022-04-02 15:06:55 +0200

    Tests insert, get & clear generic array

--- Exercise 1/src/generic_array_tests.c
+++ Exercise 1/src/generic_array_tests.c
@@ -49,6 +49,42 @@ static void test_generic_array_size_two_el(void) {
     TEST_ASSERT_EQUAL_INT(2, generic_array_size(generic_array_int));
 }
 
+static void test_generic_array_insert_one_get_first_el(void) {
+    generic_array_insert(generic_array_int, &i1);
+    TEST_ASSERT_EQUAL_PTR(&i1, generic_array_get(generic_array_int, 0));
+}
+
+static void test_generic_array_insert_two_get_two_el(void) {
+    int* exp_arr[] = {&i1, &i3};
+    generic_array_insert(generic_array_int, &i1);
+    generic_array_insert(generic_array_int, &i3);
+    int** act_arr = malloc(2 * sizeof(int*));
+    act_arr[0] = (int*)generic_array_get(generic_array_int, 0);
+    act_arr[1] = (int*)generic_array_get(generic_array_int, 1);
+    TEST_ASSERT_EQUAL_PTR_ARRAY(exp_arr, act_arr, 2);
+}
+
+static void test_generic_array_insert_three_get_last_el(void) {
+    generic_array_insert(generic_array_int, &i1);
+    generic_array_insert(generic_array_int, &i3);
+    generic_array_insert(generic_array_int, &i2);
+    TEST_ASSERT_EQUAL_PTR(&i2, generic_array_get(generic_array_int, 2));
+}
+
+static void test_generic_array_clear(void) {
+    int* el1 = (int*)malloc(sizeof(int));
+    int* el2 = (int*)malloc(sizeof(int));
+
+    *el1 = 2;
+    *el2 = 6;
+
+    generic_array_insert(generic_array_int, el1);
+    generic_array_insert(generic_array_int, el2);
+    generic_array_clear(generic_array_int);
+
+    TEST_ASSERT_EQUAL(0, generic_array_size(generic_array_int));
+}
+
 int main(void) {
     UNITY_BEGIN();
 
@@ -57,6 +93,12 @@ int main(void) {
     RUN_TEST(test_generic_array_size_one_el);
     RUN_TEST(test_generic_array_insert_two_el);
     RUN_TEST(test_generic_array_size_two_el);
+
+    RUN_TEST(test_generic_array_insert_one_get_first_el);
+    RUN_TEST(test_generic_array_insert_two_get_two_el);
+    RUN_TEST(test_generic_array_insert_three_get_last_el);
+
+    RUN_TEST(test_generic_array_clear);
 
     return UNITY_END();
 }
