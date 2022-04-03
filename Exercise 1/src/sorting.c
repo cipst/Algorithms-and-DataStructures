a115d1b6943de51b6af3a56b9a95f77e74c03485
Stefano Cipolletta <stefano.cipolletta@edu.unito.it>
2022-04-03 12:12:05 +0200

    Removing quick_sort() algorithm. Now starting implementing swap() function first (with related test)

--- Exercise 1/src/sorting.c
+++ Exercise 1/src/sorting.c
@@ -1,6 +1,7 @@
 /**
  * @file sorting.c
  * @author Stefano Cipolletta
+ * @version v0.1
  * */
 
 #include "sorting.h"
@@ -8,56 +9,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void quick_sort_wrap(GenericArray* unsorted_arrat, int (*compare)(void*, void*), unsigned long first, unsigned long last);
-
 /**
- * Hearth of the QuickSort algorithm
+ * Wrapper function for quick_sort().
+ * It helps to pass start and finish index of the unsorted_array to be sort
  * @param unsorted_array unsorted array
  * @param compare pointer to a function which determines the precedence relation between the array elements.
  * It returns 1 iff the first element is greater than the second.
  * It returns -1 iff the first element is smaller than the second.
- * It return 0 iff the first and the second elements are equal.
+ * It returns 0 iff the first and the second elements are equal.
+ * @param first index of the first element in the unsorted array
+ * @param last index of the last element in the unsorted array
+ * @author Stefano Cipolletta
+ * */
+static void quick_sort_wrap(GenericArray* unsorted_arrat, int (*compare)(void*, void*), unsigned long first, unsigned long last);
+
+/**
+ * Partition the unsorted_array.
+ * Move all the elements less than the pivot on the left of the pivot and all the elements greater than the pivot on the right.
+ * Then return the pivot index;
+ * @param unsorted_array unsorted array
+ * @param compare pointer to a function which determines the precedence relation between the array elements.
+ * It returns 1 iff the first element is greater than the second.
+ * It returns -1 iff the first element is smaller than the second.
+ * It returns 0 iff the first and the second elements are equal.
  * @param first index of the first element in the range to partition
  * @param last index of the last element in the range to partition
  * @author Stefano Cipolletta
  * */
 static unsigned long partition(GenericArray* unsorted_array, int (*compare)(void*, void*), unsigned long first, unsigned long last);
 
-void quick_sort(GenericArray* unsorted_array, int (*compare)(void*, void*)) {
+void swap(GenericArray* unsorted_array, unsigned long index1, unsigned long index2) {
     if (unsorted_array == NULL) {
         fprintf(stderr, "quick_sort(): unsorted_array parameter is NULL.\n");
         exit(EXIT_FAILURE);
     }
-    if (compare == NULL) {
-        fprintf(stderr, "quick_sort(): compare function is NULL.\n");
+    if (index1 >= generic_array_size(unsorted_array)) {
+        fprintf(stderr, "swap(%lu): index out of bound.\n", index1);
         exit(EXIT_FAILURE);
     }
-    unsigned long n = generic_array_size(unsorted_array);
-    if (n > 1)
-        quick_sort_wrap(unsorted_array, compare, 0, n - 1);
-}
-
-static void quick_sort_wrap(GenericArray* unsorted_array, int (*compare)(void*, void*), unsigned long first, unsigned long last) {
-    unsigned long p = partition(unsorted_array, compare, first, last);
-    quick_sort_wrap(unsorted_array, compare, first, p - 1);
-    quick_sort_wrap(unsorted_array, compare, p + 1, last);
-}
-
-static unsigned long partition(GenericArray* unsorted_array, int (*compare)(void*, void*), unsigned long first, unsigned long last) {
-    unsigned long i = first + 1;
-    unsigned long j = (last - first) + 1;
-
-    while (i <= j) {
-        if ((*compare)(generic_array_get(unsorted_array, i), generic_array_get(unsorted_array, first)) <= 0) {  // unsorted_array[i] <= unsorted_array[p]
-            ++i;
-        } else if ((*compare)(generic_array_get(unsorted_array, j), generic_array_get(unsorted_array, first)) > 0) {  // unsorted_array[j] > unsorted_array[p]
-            --j;
-        } else {
-            swap(unsorted_array, i, j);
-            ++i;
-            --j;
-        }
+    if (index2 >= generic_array_size(unsorted_array)) {
+        fprintf(stderr, "swap(%lu): index out of bound.\n", index2);
+        exit(EXIT_FAILURE);
     }
-    swap(unsorted_array, first, j);
-    return j;
+    void* aux = generic_array_get(unsorted_array, index1);
+    generic_array_update_at(unsorted_array, generic_array_get(unsorted_array, index2), index1);
+    generic_array_update_at(unsorted_array, aux, index2);
 }
--- Exercise 1/src/sorting_tests.c
+++ Exercise 1/src/sorting_tests.c
@@ -3,21 +3,39 @@
 #include "../../Resources/C/Unity/unity.h"
 #include "sorting.h"
 
+// Data elements that are initialized before each test
+static int i1, i2, i3;
+static GenericArray* generic_array_int;
+
 // Function called on test to initialize the environment
 void setUp(void) {
+    i1 = 9;
+    i2 = -3;
+    i3 = 1;
+    generic_array_int = generic_array_create();
 }
 
 // Function called on test to clear the environment
 void tearDown(void) {
+    generic_array_destroy(generic_array_int);
+}
+
+/* TEST FUNCTIONS */
+static void test_swap_two_el(void) {
+    int* exp_arr[] = {&i3, &i1};
+    int** act_arr = malloc(2 * sizeof(int*));
+    generic_array_insert(generic_array_int, &i1);
+    generic_array_insert(generic_array_int, &i3);
+    swap(generic_array_int, 0, 1);
+    act_arr[0] = (int*)generic_array_get(generic_array_int, 0);
+    act_arr[1] = (int*)generic_array_get(generic_array_int, 1);
+    TEST_ASSERT_EQUAL_PTR_ARRAY(act_arr, exp_arr, 2);
 }
 
 int main(void) {
     UNITY_BEGIN();
 
-    /*
-        RUN_TEST(...);
-        ...
-    */
+    RUN_TEST(test_swap_two_el);
 
     return UNITY_END();
 }
