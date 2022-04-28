77c9b606f2c1bad3c218234f733a15e5784a0092
Stefano Cipolletta <stefano.cipolletta@edu.unito.it>
2022-04-28 20:47:52 +0200

    Fixed unsigned int type on c file Skiplist + malloc for Node next + check list NULL on CreateSkipList + indexes loops

--- ex2/src/skip_list.c
+++ ex2/src/skip_list.c
@@ -1,6 +1,6 @@
 #include "./skip_list.h"
 
-Node* createNode(void* item, unsigned int level) {
+Node* createNode(void* item, int level) {
     if (item == NULL)
         return NULL;
 
@@ -12,15 +12,15 @@ Node* createNode(void* item, unsigned int level) {
 
     n->item = item;
     n->size = level;
-    n->next[level];
+    n->next = (Node**)malloc(level * sizeof(Node*));
 
     return n;
 }
 
-unsigned long randomLevel() {
-    unsigned long lvl = 1;
+int randomLevel() {
+    int lvl = 1;
 
-    srand(NULL);
+    srand((unsigned)time(NULL));
 
     while ((rand() % 2) < 0.5 && lvl < MAX_HEIGHT)
         lvl++;
@@ -30,13 +30,13 @@ unsigned long randomLevel() {
 
 SkipList* createSkipList(int (*compare)(void*, void*)) {
     SkipList* l = (SkipList*)malloc(sizeof(SkipList));
-    if (l = NULL) {
+    if (l == NULL) {
         fprintf(stderr, "createSkipList(): skiplist malloc error\n");
         return NULL;
     }
-    l->compare = compare;
     l->head = createNode(NULL, MAX_HEIGHT);
     l->max_level = 1;
+    l->compare = compare;
 }
 
 void insertSkpiList(SkipList* list, void* item) {
@@ -45,7 +45,7 @@ void insertSkpiList(SkipList* list, void* item) {
         list->max_level = n->size;
 
     Node* x = list->head;
-    for (unsigned int k = list->max_level; k >= 1; --k) {
+    for (int k = list->max_level - 1; k > 0; --k) {
         if (x->next[k] = NULL || list.compare(item, x->next[k]->item) < 0) {
             if (k < n->size) {
                 n->next[k] = x->next[k];
@@ -60,26 +60,18 @@ void insertSkpiList(SkipList* list, void* item) {
 
 void* searchSkipList(SkipList* list, void* item) {
     Node* x = list->head;
-    short int found = -1;
 
     // loop invariant: x->item < I
-    for (unsigned int i = list->max_level; i >= 1 && found == -1; --i) {
-        while (x->next != NULL && list.compare(x->next[i]->item, item) <= 0) {
-            if (list.compare(x->item, item) == 0)
-                found = 0;
+    for (int i = list->max_level - 1; i > 0; --i) {
+        while (x->next[i] != NULL && list.compare(x->next[i]->item, item) <= 0) {
             x = x->next[i];
         }
     }
 
-    if (found == 0)
-        return x->item;
+    // x->item < I <= x->next[1]->item
+    x = x->next[1];
+    if (list.compare(x->item, item) == 0)
+        return 1;
     else
-        return NULL;
-
-    // // x->item < I <= x->next[1]->item
-    // x = x->next[1];
-    // if (list.compare(x->item, item) == 0)
-    //     return x->item;
-    // else
-    //     return NULL;
+        return -1;
 }
